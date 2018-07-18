1259b4943b6e12864a96a14df3dfe0f61fcb93a9
Unknown <braydendiluca@gmail.com>
2018-07-18 10:15:41 +0700

    restructured repo

---
+++ web-server/database/migrations/2018_07_17_085624_create_temperature_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTemperatureTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temperature', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('temperature', 5, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temperature');
+    }
+}

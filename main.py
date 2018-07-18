d6e8aab930f4e28909d8c30e094c66313922d015
Maxwell Kearney <max.d.kearney@gmail.com>
2018-07-18 11:54:57 +0700

    added kekcount

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-def printkek():
+import random
+
+def printkek(kekcount):
     print('kek')
+    return kekcount + 1
 
 def main():
-    pass
+    kekcount = 0
+    for i in range(0,random.randrange(40)):
+        kekcount = printkek(kekcount)
+    print('kekcount: ' + str(kekcount))
 
 if __name__ == '__main__':
     main()
-    printkek()
